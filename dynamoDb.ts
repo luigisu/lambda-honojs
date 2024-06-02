e50ecf8bdbcdae82f2d594575bb72c3d4fae06ec
luis.fontalvo <luis.fontalvo@holcim.com>
2024-06-01 19:15:04 -0500

    :sparkles: adding dynamoDb.ts crud

---
+++ dynamoDb.ts
@@ -0,0 +1,66 @@
+import {
+    DeleteItemCommand,
+    DeleteItemCommandInput,
+    DynamoDBClient,
+    PutItemCommand,
+    PutItemInput,
+} from '@aws-sdk/client-dynamodb';
+import {
+    DynamoDBDocumentClient, GetCommandOutput, QueryCommand,
+    ScanCommand,
+    ScanCommandOutput,
+} from '@aws-sdk/lib-dynamodb';
+import {marshall} from '@aws-sdk/util-dynamodb';
+
+const client = new DynamoDBClient();
+const docClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(client);
+
+export const getDbItem = async (TableName: string, id?: string): Promise<ScanCommandOutput | GetCommandOutput> => {
+    if (id) {
+        const param = {
+            TableName,
+            KeyConditionExpression: "#id = :id",
+            ExpressionAttributeNames: {
+                "#id": "id"
+            },
+            ExpressionAttributeValues: {
+                ":id": id
+            }
+        };
+        console.log(param)
+        const command: QueryCommand = new QueryCommand(param);
+        return await docClient.send(command);
+    } else {
+        const param = {
+            TableName
+        };
+        console.log(param)
+        const command: ScanCommand = new ScanCommand(param);
+        return docClient.send(command);
+    }
+};
+export const createUpdateDbItem = (TableName: string, Attributes: any): Promise<void> => {
+    const Item: any = marshall({
+        ...Attributes,
+    });
+
+    const params: PutItemInput = {
+        TableName,
+        Item
+    };
+    const command: PutItemCommand = new PutItemCommand(params)
+    return docClient.send(command)
+}
+
+export const deleteItem = (TableName: string, Attributes: any): Promise<void> => {
+    const Key: any = marshall({
+        ...Attributes,
+    });
+    const params: DeleteItemCommandInput = {
+        Key,
+        TableName
+    };
+    const command: DeleteItemCommand = new DeleteItemCommand(params);
+    return docClient.send(command);
+}
+
--- index.ts
+++ index.ts
@@ -1,10 +1,33 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
+import {Hono} from 'hono'
+import {handle, LambdaContext, LambdaEvent} from 'hono/aws-lambda'
+import {createUpdateDbItem, deleteItem, getDbItem} from "./dynamoDb";
 
-const app = new Hono()
+type Bindings = {
+    event: LambdaEvent
+    context: LambdaContext
+}
+const TableName = process.env.USER_DB
+const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!')
+app.get('/user/:id?', async (c) => {
+    const {id}: { id: string } = c.req.param()
+    const {Items: users} = await getDbItem(TableName, id)
+    return c.json({
+        id, users
+    })
+})
+
+app.on(['POST', 'PUT'], 'user', async (c) => {
+    const body = await c.req.json()
+    await createUpdateDbItem(TableName, body)
+    return c.json({})
+})
+
+
+app.delete('/user/:id/:email', async (c) => {
+    const {id, email}: { id: string, email: string } = c.req.param()
+    await deleteItem(TableName, {id, email})
+    return c.json({})
 })
 
 export const handler = handle(app)
